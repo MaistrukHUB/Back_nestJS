31926eaa7bf30023c65e50d90267a96bab83e7d4
MaistrukHUB <vladyslavmaistruk07@gmail.com>
2023-09-24 17:22:27 +0300

    added configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from '../user/user.module';
+import { ConfigModule } from '@nestjs/config/dist';
+import configuration from '../configurations'
 
 @Module({
-  imports: [UserModule],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    load:[configuration]
+  }),UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
