78ad171e58ed920a222ef6e6670214acabf66415
MaistrukHUB <vladyslavmaistruk07@gmail.com>
2023-10-05 11:43:24 +0300

    refactoring, added try catch in services

--- src/modules/watchlist/watchlist.service.ts
+++ src/modules/watchlist/watchlist.service.ts
@@ -8,19 +8,27 @@ export class WatchlistService {
     constructor(@InjectModel(Watchlist) private readonly wathlistRepository: typeof Watchlist){}
     
     async createAsset(user, dto): Promise<CreateAssetResponse>{
-        const watchlist = {
-            user:user.id,
-            name:dto.name,
-            assetId:dto.assetId
+        try {
+            const watchlist = {
+                user:user.id,
+                name:dto.name,
+                assetId:dto.assetId
+            }
+            await this.wathlistRepository.create(watchlist)
+            return watchlist
+        } catch (error) {
+            throw new Error(error)
         }
-        await this.wathlistRepository.create(watchlist)
-        return watchlist
     }
     async deleteAsset(userId:number, assetId:string):Promise<boolean>{
+    try {
         await this.wathlistRepository.destroy({where:{
             id:assetId,
             user:userId
         }})
         return true
+    } catch (error) {
+        throw new Error(error)
+    }
     }
 }
